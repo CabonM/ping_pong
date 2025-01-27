f0d7c8256b389e3d448f0957fcfe9dc2eae7702e
CabonM <cabon.m@stjolorient.fr>
2025-01-27 15:09:39 +0100

    Update client_tcp_ping_pong.c

--- client_tcp_ping_pong.c
+++ client_tcp_ping_pong.c
@@ -16,18 +16,18 @@ void traiter_commande(int condition, const char* nom_programme, const char* mess
 }
 
 int main(int argc, char** argv) {
-    // Vérifier le nombre d'arguments
+ 
     traiter_commande(argc != 4, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'arguments");
 
-    // Vérifier le format de l'adresse IP (exemple simple)
+
     struct in_addr addr;
     traiter_commande(inet_pton(AF_INET, argv[1], &addr) != 1, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
 
-    // Vérifier le port (doit être un entier non réservé)
+ 
     int port = atoi(argv[2]);
     traiter_commande(port <= 1024 || port > 65535, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
 
-    // Initialiser une connexion TCP (fonction fictive à compléter)
+
     int socket_fd = tcp_connect(argv[1], port);
     traiter_commande(socket_fd < 0, argv[0], "Échec de la connexion au serveur");
 
@@ -35,17 +35,15 @@ int main(int argc, char** argv) {
     ssize_t sent_bytes = send(socket_fd, argv[3], strlen(argv[3]), 0);
     traiter_commande(sent_bytes < 0, argv[0], "Échec de l'envoi du message");
 
-    // Recevoir la réponse (exemple simple)
+
     char buffer[1024];
     ssize_t received_bytes = recv(socket_fd, buffer, sizeof(buffer) - 1, 0);
     traiter_commande(received_bytes < 0, argv[0], "Échec de la réception de la réponse");
 
-    // Null-terminate the received data
     buffer[received_bytes] = '\0';
     printf("Réponse du serveur: %s\n", buffer);
 
-    // Fermer la connexion
-    close(socket_fd);
+    // Fermer la connexion    close(socket_fd);
 
     exit(0);
 }
