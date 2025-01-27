ad2828df2331d0233ee9235f3e5491f8a8995e44
CabonM <cabon.m@stjolorient.fr>
2025-01-27 15:01:13 +0100

    Update udp.c

--- udp.c
+++ udp.c
@@ -10,52 +10,63 @@
 
 /* Créer une socket */
 void creer_socket(char* adresseIP, int port, SOCK* sock) {
-	sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	traiter_erreur(__FUNCTION__);
+    sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    traiter_erreur(__FUNCTION__);
 
-	// adresse et port reutilisable
-	int optval = 1;
-    	setsockopt(sock->sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-	traiter_erreur(__FUNCTION__);
+    // adresse et port reutilisable
+    int optval = 1;
+    setsockopt(sock->sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    traiter_erreur(__FUNCTION__);
 
-	/*A COMPLETER*/
+    // Initialiser la structure adresse
+    init_addr(sock);
+    sock->adresse.sin_family = AF_INET;
+    sock->adresse.sin_port = htons(port);
 
-	if (strcmp(adresseIP, "") != 0)
-		sock->adresse.sin_addr.s_addr = inet_addr(adresseIP);
-	else 
-		sock->adresse.sin_addr.s_addr = INADDR_ANY;
+    if (strcmp(adresseIP, "") != 0)
+        sock->adresse.sin_addr.s_addr = inet_addr(adresseIP);
+    else 
+        sock->adresse.sin_addr.s_addr = INADDR_ANY;
 }
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
-	/*A COMPLETER*/
-	traiter_erreur(__FUNCTION__);
+    if (bind(sock->sockfd, (struct sockaddr*)&sock->adresse, sizeof(sock->adresse)) < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
 }
 
 /*Initialiser la structure adresse */
 void init_addr(SOCK* sock) {
-	memset(&sock->adresse, 0, sizeof(sock->adresse));
+    memset(&sock->adresse, 0, sizeof(sock->adresse));
 }
 
 /* Dimensionner la file d'attente d'une socket */
 void dimensionner_file_attente_socket(int taille, SOCK* sock) {
-	listen(sock->sockfd, taille);
-	traiter_erreur(__FUNCTION__);
+    listen(sock->sockfd, taille);
+    traiter_erreur(__FUNCTION__);
 }
 
 /* Recevoir un message */
-void recevoir_message(SOCK* dst, char * buffer) {
-	/*A COMPLETER*/	
-	traiter_erreur(__FUNCTION__);
+void recevoir_message(SOCK* sock, char * buffer) {
+    socklen_t addr_len = sizeof(sock->adresse);
+    int n = recvfrom(sock->sockfd, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&sock->adresse, &addr_len);
+    if (n < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
+    buffer[n] = '\0'; // Assure que le buffer est terminé par un caractère nul
 }
 
 /* Émettre un message */
-void envoyer_message(SOCK* dst, char * message) {
-	/*A COMPLETER*/
-	traiter_erreur(__FUNCTION__);
+void envoyer_message(SOCK* sock, char * message) {
+    int n = sendto(sock->sockfd, message, strlen(message), 0, (struct sockaddr*)&sock->adresse, sizeof(sock->adresse));
+    if (n < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
 }
+
 /* Fermer la connexion */
 void fermer_connexion(SOCK* sock) {
-	close(sock->sockfd);
-	traiter_erreur(__FUNCTION__);
+    close(sock->sockfd);
+    traiter_erreur(__FUNCTION__);
 }
