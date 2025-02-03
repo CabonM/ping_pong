0e296cf6817ce791c8f296db6b385d98d88fd803
CabonM <cabon.m@stjolorient.fr>
2025-02-03 15:31:17 +0100

    Update udp.c

--- udp.c
+++ udp.c
@@ -1,6 +1,5 @@
 #include "erreur.h"
 #include "udp.h"
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -9,64 +8,102 @@
 #include <arpa/inet.h>
 
 /* Créer une socket */
-void creer_socket(char* 10.0.20.15, 8080, SOCK* sock) {
+void creer_socket(char* adresseIP, int port, SOCK* sock) {
+    // Créer une socket UDP
     sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    traiter_erreur(__FUNCTION__);
-
-    /* adresse et port reutilisable */
-    int optval = 1;
-    setsockopt(sock->sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-    traiter_erreur(__FUNCTION__);
-
-    /* Initialiser la structure adresse */
-    init_addr(sock);
-    sock->adresse.sin_family = AF_INET;
-    sock->adresse.sin_port = htons(port);
-
-    if (strcmp(adresseIP, "") != 0)
-        sock->adresse.sin_addr.s_addr = inet_addr(10.0.20.15);
-    else 
-        sock->adresse.sin_addr.s_addr = INADDR_ANY;
+    if (sock->sockfd < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
+    
+    // Initialiser la structure d'adresse
+    memset(&sock->addr, 0, sizeof(sock->addr));
+    sock->addr.sin_family = AF_INET;
+    sock->addr.sin_port = htons(port);
+    sock->addrlen = sizeof(struct sockaddr_in);
+    
+    // Si adresseIP est NULL, on utilise INADDR_ANY
+    if (adresseIP == NULL) {
+        sock->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    } else {
+        sock->addr.sin_addr.s_addr = inet_addr(adresseIP);
+    }
 }
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
-    if (bind(sock->sockfd, (struct sockaddr*)&sock->adresse, sizeof(sock->adresse)) < 0) {
+    int result = bind(sock->sockfd, (struct sockaddr*)&sock->addr, sock->addrlen);
+    if (result < 0) {
         traiter_erreur(__FUNCTION__);
     }
 }
 
-/*Initialiser la structure adresse */
+/* Initialiser la structure adresse client */
 void init_addr(SOCK* sock) {
-    memset(&sock->adresse, 0, sizeof(sock->adresse));
+    memset(&sock->addr, 0, sizeof(sock->addr));
+    sock->addr.sin_family = AF_INET;
+    sock->addrlen = sizeof(struct sockaddr_in);
 }
 
 /* Dimensionner la file d'attente d'une socket */
 void dimensionner_file_attente_socket(int taille, SOCK* sock) {
-    listen(sock->sockfd, taille);
-    traiter_erreur(__FUNCTION__);
+    // Cette fonction n'est pas nécessaire pour UDP car il n'y a pas de file d'attente
+    (void)taille; // Pour éviter l'avertissement de variable non utilisée
+    (void)sock;   // Pour éviter l'avertissement de variable non utilisée
 }
 
 /* Recevoir un message */
-void recevoir_message(SOCK* sock, char * buffer) {
-    socklen_t addr_len = sizeof(sock->adresse);
-    int n = recvfrom(sock->sockfd, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&sock->adresse, &addr_len);
-    if (n < 0) {
+void recevoir_message(SOCK* dst, char* buffer) {
+    ssize_t nbytes = recvfrom(dst->sockfd, buffer, TAILLE_BUFFER - 1, 0,
+                             (struct sockaddr*)&dst->addr, (socklen_t*)&dst->addrlen);
+    if (nbytes < 0) {
         traiter_erreur(__FUNCTION__);
     }
-    buffer[n] = '\0'; // Assure que le buffer est terminé par un caractère nul
+    buffer[nbytes] = '\0'; // Assurer que le message est terminé par un caractère nul
 }
 
 /* Émettre un message */
-void envoyer_message(SOCK* sock, char * message) {
-    int n = sendto(sock->sockfd, message, strlen(message), 0, (struct sockaddr*)&sock->adresse, sizeof(sock->adresse));
-    if (n < 0) {
+void envoyer_message(SOCK* dst, char* message) {
+    ssize_t nbytes = sendto(dst->sockfd, message, strlen(message), 0,
+                           (struct sockaddr*)&dst->addr, dst->addrlen);
+    if (nbytes < 0) {
         traiter_erreur(__FUNCTION__);
     }
 }
 
 /* Fermer la connexion */
 void fermer_connexion(SOCK* sock) {
-    close(sock->sockfd);
-    traiter_erreur(__FUNCTION__);
+    if (close(sock->sockfd) < 0) {
+        traiter_erreur(__FUNCTION__);
+    }
+}
+
+/* Fonctions de validation */
+int est_ip_valide(const char *ip) {
+    if (ip == NULL) return 0;
+    
+    int segments = 0;
+    int digits = 0;
+    int value = 0;
+    
+    while (*ip) {
+        if (*ip == '.') {
+            if (digits == 0) return 0;
+            segments++;
+            digits = 0;
+            value = 0;
+        } else if (*ip >= '0' && *ip <= '9') {
+            value = value * 10 + (*ip - '0');
+            digits++;
+            if (value > 255 || digits > 3) return 0;
+        } else {
+            return 0;
+        }
+        ip++;
+    }
+    
+    return segments == 3 && digits > 0;
+}
+
+int est_port_valide(int port) {
+    return port >= 1024 && port <= 65535;  // Ports non privilégiés
 }
