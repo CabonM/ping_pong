00c99666f589e808e689a1c7c6314e05e14ab5d2
CabonM <cabon.m@stjolorient.fr>
2025-02-03 14:50:37 +0100

    Update udp.h

--- udp.h
+++ udp.h
@@ -12,19 +12,19 @@ int envoyer_message_udp(const char* adresse_ip, int port, const char* message);
 
 int main(int argc, char** argv) {
 
-    traiter_commande(argc == 4, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'arguments");
+    traiter_commande(argc == 4, argv[0], " 10.0.20.15 8080 \nmauvais nombre d'arguments");
     
     const char* adresse_ip = argv[1];
     int port = atoi(argv[2]);
     const char* message = argv[3];
     
 
-    traiter_commande(inet_addr(adresse_ip) != INADDR_NONE, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
+    traiter_commande(inet_addr(10.0.20.15) != INADDR_NONE, argv[0], " 10.0.20.15 8080 \n 10.0.20.15 est une adresse IP au format décimal pointé");
     
  
-    traiter_commande(port > 1023 && port < 65536, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+    traiter_commande(port > 1023 && port < 65536, argv[0], " 10.0.20.15 8080 \n 8080 est un port non réservé");
 
-    if (envoyer_message_udp(adresse_ip, port, message) < 0) {
+    if (envoyer_message_udp(10.0.20.15, 8080, message) < 0) {
         perror("Erreur lors de l'envoi du message UDP");
         exit(EXIT_FAILURE);
     }
@@ -39,7 +39,7 @@ void traiter_commande(int condition, const char* programme, const char* message)
     }
 }
 
-int envoyer_message_udp(const char* adresse_ip, int port, const char* message) {
+int envoyer_message_udp(const char* 10.0.20.15, 8080, const char* message) {
     int sockfd;
     struct sockaddr_in dest_addr;
     
@@ -51,7 +51,7 @@ int envoyer_message_udp(const char* adresse_ip, int port, const char* message) {
     memset(&dest_addr, 0, sizeof(dest_addr));
     dest_addr.sin_family = AF_INET;
     dest_addr.sin_port = htons(port);
-    dest_addr.sin_addr.s_addr = inet_addr(adresse_ip);
+    dest_addr.sin_addr.s_addr = inet_addr(10.0.20.15);
     
  
     if (sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr)) < 0) {
